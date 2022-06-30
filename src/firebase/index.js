66c2beac85e270396fba76b09af19a55132d78b6
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-06-30 10:47:48 +0200

    Firebase configuration

---
+++ src/firebase/index.js
@@ -0,0 +1,10 @@
+import { initializeApp } from 'firebase/app';
+
+const app = initializeApp({
+  apiKey: process.env.REACT_APP_FIREVASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREVASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREVASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREVASE_STORAG_EBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREVASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREVASE_APP_ID,
+});
