690c089b2e9981a375555ab2efc2e0cbc9139691
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-06-30 15:39:27 +0200

    Navigation bar changes

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,15 +7,16 @@ import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
-import AddIcon from '@mui/icons-material/Add';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
 import Logo from '../../assets/logo.jpg';
 
 const settings = ['My Profile', 'Settings', 'Logout'];
 
 export default function NavBar() {
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const isLogin = false;
+  const [anchorElUser, setAnchorElUser] = useState(null);
   const handleOpenUserMenu = (event) => {
     setAnchorElUser(event.currentTarget);
   };
@@ -24,54 +25,61 @@ export default function NavBar() {
   };
 
   return (
-    <AppBar
-      sx={{ backgroundColor: '#000' }}
-      enableColorOnDark
-      position="static"
-    >
+    <AppBar color="background" enableColorOnDark position="static">
       <Container maxWidth="md">
         <Toolbar
           sx={{ display: 'flex', justifyContent: 'space-between' }}
           disableGutters
         >
           <Box component="img" src={Logo} sx={{ height: '50px', mr: 1 }} />
-          <Box>
-            <Tooltip title="Add new meme">
-              <IconButton
-                sx={{ color: '#fff', border: '1px solid #fff', mr: 1 }}
-                aria-label="delete"
+          {isLogin ? (
+            <Box>
+              <Button color="primary" variant="contained" sx={{ mr: 1 }}>
+                Add meme
+              </Button>
+              <Tooltip title="Open settings">
+                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                  <Avatar alt="User avatar" src="" />
+                </IconButton>
+              </Tooltip>
+              <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorEl={anchorElUser}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'left',
+                }}
+                open={Boolean(anchorElUser)}
+                onClose={handleCloseUserMenu}
               >
-                <AddIcon />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="User avatar" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
+                {settings.map((setting) => (
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                    <Typography textAlign="center">{setting}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+          ) : (
+            <Box>
+              <Button
+                sx={{
+                  mr: 1,
+                }}
+                variant="contained"
+              >
+                Log in
+              </Button>
+              <Button color="secondary" variant="contained">
+                Sign in
+              </Button>
+            </Box>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
