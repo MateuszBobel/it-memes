e77fe5dda6dbd1fe5419a666223f6ab74a81a078
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-06-30 10:44:35 +0200

    Change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,20 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-  plugins: ["react"],
+  plugins: ['react'],
   rules: {
-    "react/react-in-jsx-scope": "off",
+    'no-unused-vars': [
+      'warn',
+      { vars: 'all', args: 'after-used', ignoreRestSiblings: false },
+    ],
+    'react/react-in-jsx-scope': 'off',
   },
 };
