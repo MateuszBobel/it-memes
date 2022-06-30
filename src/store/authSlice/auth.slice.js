4a602ad60eb55a904922497b32f526249a9b80c8
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-06-30 15:53:15 +0200

    Auth reducer

---
+++ src/store/authSlice/auth.slice.js
@@ -0,0 +1,76 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const authSlice = createSlice({
+  name: 'auth',
+  initialState: {
+    isLoaded: true,
+    user: null,
+    isLogin: false,
+    fetchUserError: null,
+    fetchUserLoading: false,
+
+    loginUserLoading: false,
+    loginUserError: null,
+
+    registerUserLoading: false,
+    registerUserError: null,
+
+    resetPasswordLoading: false,
+    resetPasswordError: null,
+
+    logoutUserLoading: false,
+    logoutUserError: null,
+  },
+  reducers: {
+    setUser(state, action) {
+      state.user = action.payload;
+      state.isLoaded = true;
+      state.isLogin = true;
+    },
+    fetchUserLoading(state, action) {
+      state.fetchUserLoading = action.payload;
+    },
+    fetchUserError(state, action) {
+      state.fetchUserError = action.payload;
+    },
+    loginUserLoading(state, action) {
+      state.loginUserLoading = action.payload;
+    },
+    loginUserError(state, action) {
+      state.loginUserError = action.payload;
+    },
+    registerUserLoading(state, action) {
+      state.registerUserLoading = action.payload;
+    },
+    registerUserError(state, action) {
+      state.registerUserError = action.payload;
+    },
+    resetPasswordLoading(state, action) {
+      state.resetPasswordLoading = action.payload;
+    },
+    resetPasswordError(state, action) {
+      state.resetPasswordError = action.payload;
+    },
+    logoutUserLoading(state, action) {
+      state.resetPasswordLoading = action.payload;
+    },
+    logoutUserError(state, action) {
+      state.resetPasswordError = action.payload;
+    },
+  },
+});
+
+export const {
+  setUser,
+  fetchUserLoading,
+  fetchUserError,
+  loginUserLoading,
+  loginUserError,
+  registerUserLoading,
+  registerUserError,
+  resetPasswordLoading,
+  resetPasswordError,
+  logoutUserLoading,
+  logoutUserError,
+} = authSlice.actions;
+export default authSlice.reducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import authSlice from './authSlice/auth.slice';
 
 export default configureStore({
-  reducer: {},
+  reducer: {
+    auth: authSlice,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({ serializableCheck: false }),
 });
