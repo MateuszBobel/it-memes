ef97e87a8973e3aa50eab230a97853300c10228d
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-06-30 15:55:15 +0200

    Add custom theme

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import CssBaseline from '@mui/material/CssBaseline';
+import CustomThemeProvider from './providers/CustomThemeProvider';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from './store/index';
@@ -10,12 +11,14 @@ import store from './store/index';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <CustomThemeProvider>
       <CssBaseline />
-      <Router>
-        <App />
-      </Router>
-    </Provider>
+      <Provider store={store}>
+        <Router>
+          <App />
+        </Router>
+      </Provider>
+    </CustomThemeProvider>
   </React.StrictMode>
 );
 
