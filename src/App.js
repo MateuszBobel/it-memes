6a7a6d1ee693d177960b32afb2c92b47f1b4bef1
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-06-30 15:54:34 +0200

    Add loading screen to App comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import Dashboard from './views/Dashboard';
+import LoadingScreen from './components/LoadingScreen';
 
 function App() {
+  const isLoaded = useSelector((state) => state.auth.isLoaded);
+
+  if (!isLoaded) {
+    return <LoadingScreen />;
+  }
+
   return (
     <Routes>
       <Route element={<Dashboard />} path="/" />
