6cd64188f4c32b19b346f7fcdea535aa1ce74d9d
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-06-30 11:21:55 +0200

    Import styles reset

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
+import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <CssBaseline />
     <Router>
       <App />
     </Router>
