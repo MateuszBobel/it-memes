18808e145a3d498adc7d15bcab5e8d3a3b92526a
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-07-04 09:49:25 +0200

    Add user fetch action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUser } from './store/authSlice/auth.actions';
 import Dashboard from './views/Dashboard';
 import Login from './views/Login';
 import PublicRote from './hoc/PublicRoute';
 import LoadingScreen from './components/LoadingScreen';
 
 function App() {
-  const isLoaded = useSelector((state) => state.auth.isLoaded);
+  const dispatch = useDispatch();
+  const { isLoaded } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    dispatch(fetchUser());
+  }, []);
 
   if (!isLoaded) {
     return <LoadingScreen />;
