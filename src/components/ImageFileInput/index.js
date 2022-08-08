800f43a2cdf8b19cee703c93f8ed5fdf71904ed8
Mateusz Bobel <mateusz.bobel.mb@gmail.com>
2022-08-08 11:34:44 +0200

    Add error message to image file input

--- src/components/ImageFileInput/index.js
+++ src/components/ImageFileInput/index.js
@@ -4,45 +4,65 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 
-export default function ImageFileInput({ file, onChange, name }) {
+export default function ImageFileInput({
+  file,
+  onChange,
+  name,
+  error,
+  helperText,
+}) {
   return (
-    <Button
-      component="label"
-      sx={{
-        minHeight: '150px',
-        width: '100%',
-        margin: '8px 0 4px 0',
-        border: !file && '1px dashed',
-        borderColor: 'darkgray',
-      }}
-    >
-      {file ? (
-        <Box
-          sx={{ width: '100%' }}
-          component="img"
-          src={URL.createObjectURL(file)}
+    <>
+      <Button
+        component="label"
+        sx={{
+          minHeight: '150px',
+          width: '100%',
+          margin: '8px 0 4px 0',
+          border: !file && '1px dashed',
+          borderColor: error ? '#d32f2f' : 'darkgray',
+          color: 'darkgray',
+        }}
+      >
+        {file ? (
+          <Box
+            sx={{ width: '100%' }}
+            component="img"
+            src={URL.createObjectURL(file)}
+          />
+        ) : (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <AddPhotoAlternateOutlinedIcon />
+            <Typography>CLick here to add image</Typography>
+          </Box>
+        )}
+        <input
+          name={name}
+          onChange={onChange}
+          hidden
+          accept="image/*"
+          type="file"
         />
-      ) : (
-        <Box
+      </Button>
+      {error && (
+        <Typography
           sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
+            color: '#d32f2f',
+            margin: '0 14px',
           }}
+          variant="caption"
         >
-          <AddPhotoAlternateOutlinedIcon />
-          <Typography>CLick here to add image</Typography>
-        </Box>
+          {helperText}
+        </Typography>
       )}
-      <input
-        name={name}
-        onChange={onChange}
-        hidden
-        accept="image/*"
-        type="file"
-      />
-    </Button>
+    </>
   );
 }
 
@@ -59,9 +79,13 @@ ImageFileInput.propTypes = {
   ]),
   onChange: PropTypes.func.isRequired,
   name: PropTypes.string,
+  error: PropTypes.bool,
+  helperText: PropTypes.string,
 };
 
 ImageFileInput.defaultProps = {
   name: 'file',
   file: null,
+  error: false,
+  helperText: '',
 };
